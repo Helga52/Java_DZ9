7e632e771eb9b3928f9848f78600915272bfe9e1
Olga <helga_ser@mail.ru>
2023-08-10 19:58:13 +0300

    Initial

---
+++ src/main/java/ru/netology/javaqa/FormDate.java
@@ -0,0 +1,7 @@
+package ru.netology.javaqa;
+
+public class FormDate {
+    public int day; // поле дня
+    public int month; // поле месяца
+    public int year; // поле года
+}
---
+++ src/main/java/ru/netology/javaqa/Post.java
@@ -0,0 +1,11 @@
+package ru.netology.javaqa;
+
+public class Post {
+    public String name; // поле имени
+    public String patronymic; // поле отчества
+    public String surname; // поле фамилии
+    public String passport; // поле серии и номера паспорта
+    public String phone; // поле номера телефона
+    public Boolean subscription; // поле согласия о получении информации
+    public FormDate birthday; // поле день рождения
+}
