b04f352e8975e55c4fb0f12d126ae3e14c2de677
Olga <helga_ser@mail.ru>
2023-08-10 20:28:46 +0300

    Change output order

--- src/main/java/ru/netology/javaqa/Post.java
+++ src/main/java/ru/netology/javaqa/Post.java
@@ -1,11 +1,11 @@
 package ru.netology.javaqa;
 
 public class Post {
-    public String name; // поле имени
-    public String patronymic; // поле отчества
-    public String surname; // поле фамилии
-    public String passport; // поле серии и номера паспорта
-    public String phone; // поле номера телефона
-    public Boolean subscription; // поле согласия о получении информации
     public FormDate birthday; // поле день рождения
+    public String name; // поле имени
+    public String passport; // поле серии и номера паспорта
+    public String patronymic; // поле отчества
+    public String phone; // поле номера телефона
+    public String surname; // поле фамилии
+    public Boolean subscription; // поле согласия о получении информации
 }
